c4b656b50f0a6405f43dafc4aa81b670b7116c01
anujdhiman485 <anujdhiman2005@gmail.com>
2026-01-11 13:35:00 +0530

    About Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from "./components/Footer/Footer"
 import Navbar from "./components/Navbar/Navbar"
 import Skills from "./components/Skills/Skills"
 import Work from "./components/Work/work"
+import BlurBlob from "./components/BlurBlob"
+
 function App() {
  
 
@@ -13,7 +15,7 @@ function App() {
     <>
          <div className="bg-[#050414]">
 
-       
+       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       
